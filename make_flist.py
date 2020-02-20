751e757cd77a5f383b2bc8db993d5051341f63b4
kazuysh <kazuysh@jp.ibm.com>
2020-02-20 05:20:22 +0000

    push test

--- make_flist.py
+++ make_flist.py
@@ -1,12 +1,13 @@
 import glob
 import pandas as pd
 
-matchPath = glob.glob('../jpg_data/*.jpg', recursive=True)
+matchPath = glob.glob('/mnt/*/*/2020*/**/*.jpg', recursive=True)
+#matchPath = glob.glob('/mnt/**/*.jpg', recursive=True)
 df=pd.DataFrame(matchPath,columns=['Path'])
 df=df['Path'].str.split('/',expand=True)
 
-df['Name']=df[2].str.replace('.jpg','')
-df=df.set_index("Name")
-df.drop(0,axis=1)
+#df['Name']=df[2].str.replace('.jpg','')
+#df=df.set_index("Name")
+#df.drop(0,axis=1)
 
 df.to_csv('FileList.csv')
